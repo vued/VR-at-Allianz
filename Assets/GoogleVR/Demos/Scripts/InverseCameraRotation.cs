84c2b0a7a1e0928cd6205774dc2d9dabd60e6c24
David Möller <moelldav@gmail.com>
2017-06-18 19:44:55 +0200

    Rotation fixed
    Logo needs more work

--- Assets/GoogleVR/Demos/Scripts/InverseCameraRotation.cs
+++ Assets/GoogleVR/Demos/Scripts/InverseCameraRotation.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class InverseCameraRotation : MonoBehaviour {
 
-	public Transform relativeTransform;
+	public Vector3 fixedRotation;
 
 	// Use this for initialization
 	void Start () {
@@ -13,19 +13,11 @@ public class InverseCameraRotation : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		float x = relativeTransform.localRotation.x;
-		float y = relativeTransform.localRotation.y;
-		float z = relativeTransform.localRotation.z;
-		float w = relativeTransform.localRotation.w;
-		Debug.Log("Relative Rotation: " + x + "; " + y + "; " + z + "; " + w);
-		this.transform.localRotation.Set (x, y, z, w);
+	void LateUpdate () {
 
-		x = this.transform.localRotation.x;
-		y = this.transform.localRotation.y;
-		z = this.transform.localRotation.z;
-		w = this.transform.localRotation.w;
-		Debug.Log("Logo     Rotation: " + x + "; " + y + "; " + z + "; " + w);
+		// reorientate to a fixed direction
+		fixedRotation.Normalize ();
+		this.transform.rotation = Quaternion.LookRotation(fixedRotation);
 
 	}
 }
