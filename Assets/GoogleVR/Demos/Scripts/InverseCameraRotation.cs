5f78ff8efdfd05d2a7a1be3cf7ee604418bde14f
David Möller <moelldav@gmail.com>
2017-06-18 19:06:20 +0200

    Allianz Logo and first attempt for a rotation-lock script
    script does not work at the moment.

---
+++ Assets/GoogleVR/Demos/Scripts/InverseCameraRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InverseCameraRotation : MonoBehaviour {
+
+	public Transform relativeTransform;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = relativeTransform.localRotation.x;
+		float y = relativeTransform.localRotation.y;
+		float z = relativeTransform.localRotation.z;
+		float w = relativeTransform.localRotation.w;
+		Debug.Log("Relative Rotation: " + x + "; " + y + "; " + z + "; " + w);
+		this.transform.localRotation.Set (x, y, z, w);
+
+		x = this.transform.localRotation.x;
+		y = this.transform.localRotation.y;
+		z = this.transform.localRotation.z;
+		w = this.transform.localRotation.w;
+		Debug.Log("Logo     Rotation: " + x + "; " + y + "; " + z + "; " + w);
+
+	}
+}
